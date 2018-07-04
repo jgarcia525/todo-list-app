3238a7a16b07dfab89c00c1a216c13d38850d0c5
Jorge Garcia <jorgegarcia525@gmail.com>
2018-07-04 16:26:00 -0500

    added todo creation

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,12 +1,22 @@
 // Check off specific todos by clicking
-$("li").click(function() {
+$("ul").on("click", "li", function() {
   // if li is gray
   $(this).toggleClass("completed");
 });
 
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
 	$(this).parent().fadeOut(500, function() {
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+  	// grabing new todo text from input
+  	var input = $(this).val();
+  	$(this).val("");
+  	// create a new li and dd to ul
+  	$("ul").append("<li><span>X</span> " + input +  "</li>");
+  }
 });
